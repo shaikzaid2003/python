cc04fed8fc1678adad4218c7d306028e57bf2876
S ZAID <119155933+shaikzaid2003@users.noreply.github.com>
2022-12-06 14:27:33 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,24 @@
+'''email = input("Enter Your Email: ").strip()
+username = email[:email.index("@")]
+domain_name = email[email.index("@")+1:]
+print('Your user name is:',username,'and your domain is:',domain_name.upper())'''
+'''def myfunc():
+    global x
+    x="boy"
+myfunc()
+print("zaid is a good "+x)
+
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def myfunc():'''
+fruits=["apple","banana","cherry"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+        
+        
+        
+        
